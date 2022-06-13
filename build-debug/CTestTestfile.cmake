26e781dbf5df564b79923172a72bc30b837782fc
roderickwr <r.wallrath@t-online.de>
2022-06-13 20:40:52 +0200

    initial commit

---
+++ build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/roderich/Documents/BAP
+# Build directory: /home/roderich/Documents/BAP/build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("check")
