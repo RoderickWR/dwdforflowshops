9b1e47c6c7b0e56d1e3b0a686ed229d79b5b1733
roderickwr <r.wallrath@t-online.de>
2022-10-26 21:21:39 +0200

    finish branchingexecLP

--- src/branch_ryanfoster.c
+++ src/branch_ryanfoster.c
@@ -150,10 +150,10 @@ SCIP_DECL_BRANCHEXECLP(branchExeclpRyanFoster)
    SCIP_CALL( SCIPgetLPBranchCands(scip, &lpcands, NULL, &lpcandsfrac, NULL, &nlpcands, NULL) );
    assert(nlpcands > 0);
    vardata = SCIPvarGetData(lpcands[0]);
-   nconsids_main = SCIPvardataGetNConsids(vardata); // finds out  to which machine candidate[0] belongs to => branch on that machine
+   nconsids_main = SCIPvardataGetNConsids(vardata); // finds out to which machine candidate[0] belongs to => branch on that machine
    float ratio_branches_new = 0;
    float ratio_branches = 0;
-   int i_found = -1;
+   int i_found = -1; // which is the order of jobs i,j to branch on?
    int j_found = -1;
 
    for( i = 0; i < nbrJobs; ++i ) {
@@ -192,88 +192,17 @@ SCIP_DECL_BRANCHEXECLP(branchExeclpRyanFoster)
       }
    }
 
-
-   /* compute weights for each order pair */
-   for( v = 0; v < nlpcands; ++v )
-   {
-      SCIP_Real solval;
-
-      assert(lpcands[v] != NULL);
-
-      solval = lpcandsfrac[v];
-
-      /* get variable data which contains the information to which constraints/items the variable belongs */
-      vardata = SCIPvarGetData(lpcands[v]);
-
-      nconsids = SCIPvardataGetNConsids(vardata);
-
-      /* loop over all constraints/items the variable belongs to */
-      for( i = 0; i < nconsids; ++i )
-      {
-         id1 = consids[i];
-
-         /* store the LP sum for single items in the diagonal */
-         pairweights[id1][id1] += solval;
-
-         /* update LP sums for all pairs of items */
-         for( j = i+1; j < nconsids; ++j )
-         {
-            id2 = consids[j];
-            assert(id1 < id2);
-
-            pairweights[id2][id1] += solval;
-
-            assert( SCIPisFeasGE(scip, pairweights[id2][id1], 0.0) );
-         }
-      }
+   if (i_found == -1 & j_found == -1) {
+      return SCIP_CUTOFF; // CUTOFF this node if no order to branch on
    }
-
-   /* select branching */
-   bestvalue = 0.0;
-   id1 = -1;
-   id2 = -1;
-
-   for( i = 0; i < nitems; ++i )
-   {
-      for( j = 0; j < i; ++j )
-      {
-         value = MIN(pairweights[i][j], 1-pairweights[i][j]);
-
-         if( bestvalue < value )
-         {
-            /* there is no variable with (fractional) LP value > 0 that contains exactly one of the items */
-            if( SCIPisEQ(scip, pairweights[i][j], pairweights[i][i])
-               && SCIPisEQ(scip, pairweights[i][j], pairweights[j][j]) )
-               continue;
-
-            bestvalue = value;
-            id1 = j;
-            id2 = i;
-         }
-      }
-   }
-
-   assert( bestvalue > 0.0 );
-   assert( id1 >= 0 && id1 < nitems);
-   assert( id2 >= 0 && id2 < nitems);
-
-   /* free memory for triangle matrix */
-   for( i = 0; i < nitems; ++i )
-   {
-      SCIPfreeBufferArray(scip, &pairweights[i]);
-   }
-   SCIPfreeBufferArray(scip, &pairweights);
-
-   SCIPdebugMsg(scip, "branch on order pair <%d,%d> with weight <%g>\n",
-      SCIPprobdataGetIds(probdata)[id1], SCIPprobdataGetIds(probdata)[id2], bestvalue);
 
    /* create the branch-and-bound tree child nodes of the current node */
    SCIP_CALL( SCIPcreateChild(scip, &childsame, 0.0, SCIPgetLocalTransEstimate(scip)) );
    SCIP_CALL( SCIPcreateChild(scip, &childdiffer, 0.0, SCIPgetLocalTransEstimate(scip)) );
 
    /* create corresponding constraints */
-   SCIP_CALL( SCIPcreateConsSamediff(scip, &conssame, "same", id1, id2, SAME, childsame, TRUE) );
-   SCIP_CALL( SCIPcreateConsSamediff(scip, &consdiffer, "differ", id1, id2, DIFFER, childdiffer, TRUE) );
+   SCIP_CALL( SCIPcreateConsSamediff(scip, &conssame, "same", i_found, j_found, SAME, childsame, TRUE) );
+   SCIP_CALL( SCIPcreateConsSamediff(scip, &consdiffer, "differ", i_found, j_found, DIFFER, childdiffer, TRUE) );
 
   /* add constraints to nodes */
    SCIP_CALL( SCIPaddConsNode(scip, childsame, conssame, NULL) );
