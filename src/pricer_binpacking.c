d0109881cda2b39801a904109fb7dcd907e0265c
roderickwr <r.wallrath@t-online.de>
2022-12-04 21:59:48 +0100

    fixed position of allSubsOptimal

--- src/pricer_binpacking.c
+++ src/pricer_binpacking.c
@@ -713,7 +713,7 @@ SCIP_DECL_PRICERREDCOST(pricerRedcostBinpacking)
    schedule* s1;
    SCIP_VAR*** lambArr;
    int* ids;
-   SCIP_Bool addvar;
+   SCIP_Bool addvar = FALSE;
    SCIP_Bool allSubsOptimal = TRUE;
    char buf[256];
 
@@ -824,9 +824,7 @@ SCIP_DECL_PRICERREDCOST(pricerRedcostBinpacking)
       }
 
       sols = SCIPgetSols(subscip[i]);
-      nsols = SCIPgetNSols(subscip[i]);
-      addvar = FALSE;
-   
+      nsols = SCIPgetNSols(subscip[i]);   
       /* loop over all solutions and create the corresponding column to master if the reduced cost are negative for master,
       * that is the objective value i greater than 1.0
       */
@@ -951,16 +949,14 @@ SCIP_DECL_PRICERREDCOST(pricerRedcostBinpacking)
       SCIPfreeBufferArray(scip, &orderVars); /*free for order vars */
       SCIPfreeBufferArray(scip, &endVars );
       SCIPfreeBufferArray(scip, &startVars ); /*free for start and finish vars */
-      
-      
-
-      if( addvar || allSubsOptimal ) // a variable was added by a sub problem or all sub problems are optimal and not variable was added
-         (*result) = SCIP_SUCCESS;
 
       /* free sub SCIP */
       SCIP_CALL( SCIPfree(&subscip[i]) );
    }
 
+   if( addvar || allSubsOptimal ) { // a variable was added by a sub problem or all sub problems are optimal and not variable was added
+   (*result) = SCIP_SUCCESS;
+   }
    // /* free pricer MIP */
    // SCIPfreeBufferArray(scip, &vars);
    printf("Ending DECL_PRICERREDCOST()\n");
