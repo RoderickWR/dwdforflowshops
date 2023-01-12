71b2c67b401e9664b20c19dcb70df22e9c2b61f9
roderickwr <r.wallrath@t-online.de>
2023-01-12 21:49:43 +0100

    compile c from src, create shared library, execute in py

--- mainP.py
+++ mainP.py
@@ -1,20 +1,25 @@
 # build.py
 from distutils.ccompiler import new_compiler
 import subprocess
-
-if __name__ == '__main__':
-    compiler = new_compiler()
-    compiler.compile(['mainC.c'])
-    compiler.link_executable(['mainC.o'], 'main')
-    subprocess.call()
-
-
-
-# ctypes_test.py
 import ctypes
 import pathlib
 
-if __name__ == "__main__":
+#%% 
+if __name__ == '__main__':
+    # compiler = new_compiler()
+    # compiler.compile(['mainC.c'])
+    # compiler.link_executable(['mainC.o'], 'main')
+    # subprocess.call()
+    command = "gcc -c mainC.c "
+    subprocess.run(command, shell = True)
+
+
+    #%% 
+    # cmd = ["gcc", "mainC.o", "-shared", "-o" , "libmainC.so"]
+    command = "gcc mainC.o -shared -o libmainC.so"
+    subprocess.run(command, shell = True)
+    #%% 
+
     # Load the shared library into ctypes
     libname = pathlib.Path().absolute() / "libmainC.so"
     c_lib = ctypes.CDLL(libname)
