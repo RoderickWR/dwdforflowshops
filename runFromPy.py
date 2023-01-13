82b2b6fbd1c9c7c008ea499999515047a05392f4
roderickwr <r.wallrath@t-online.de>
2023-01-13 15:36:55 +0100

    finalize gantt plotter 1

--- runFromPy.py
+++ runFromPy.py
@@ -7,17 +7,21 @@ import os
 import pandas as pd
 import plotly.express as px
 import plotly.figure_factory as ff
+import random
 
 #%% 
 if __name__ == '__main__':
 
+    # CALL OPTIMIZER
     assert(os.getcwd() == '/home/roderich/Documents/BAP/gitdir02/dwdforflowshops')
     os.chdir("./build")
 
     command = "./binpacking"
     subprocess.run(command, shell = True)
+
     # LOAD raw gantt data after opt
     df = pd.read_csv('GanttRawData.csv')
+
     # PREPROCESS raw gantt data for time format
     ref_date = pd.DataFrame({'Date': ['2023-01-01-00-00-00']})
     ref_date = pd.to_datetime(ref_date.Date, format='%Y-%m-%d-%H-%M-%S')
@@ -27,9 +31,20 @@ if __name__ == '__main__':
 
     df["Task"] = "Machine: " + df["Machine"].astype(str) + "   Job: " + df["Job"].astype(str)
 
-    fig = ff.create_gantt(df)
+    
 
 
-    # fig = px.bar(df, x="Start", y="Job", title= "test")
+    # PLOT gantt chart 
+
+    # create colormap
+    colors={}
+    random.seed(100)
+    
+    for i in range(max(df['Job'])+1):
+        colors[i] = 'rgb(%s,%s,%s)'%(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
+        
+    fig = ff.create_gantt(df, colors=colors, index_col='Job')
+
     fig.show()
 
